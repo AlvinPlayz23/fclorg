c44b5e39390d5ebb6a3e06bc7e1000ac7a707256
AlvinPlayz23 <162082948+AlvinPlayz23@users.noreply.github.com>
2024-06-14 20:14:30 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,8 +25,8 @@ async function fetchData(sheetId, query) {
 }
 
 async function initialize() {
-    const teamsSheetId = '2PACX-1vTljrUytTLqraazPmrhWPhHXa0GIGyC540ED9UZfdaHMtUxvqAisptUUHOBDBSi-nhpfAo1sLgURDEd';
-    const ovrSheetId = 'YOUR_OVR_SHEET_ID';
+    const teamsSheetId = '11QkdTr062hCnWbS5P_WI36FE244-TkNzjbDMVcz06WQ';
+    const ovrSheetId = '1p8MmocAesqYHzORRy4xAxa6wDPnegYBVDnpXp0IjVJc';
     const tournamentsSheetId = 'YOUR_TOURNAMENT_SHEET_ID';
     const leaguesSheetId = 'YOUR_LEAGUE_SHEET_ID';
     const newsSheetId = 'YOUR_NEWS_SHEET_ID';
